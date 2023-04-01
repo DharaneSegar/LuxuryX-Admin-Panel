bce9e823bbffcdc6bab1b32021c695906d917c3f
Dharane <it21068478@my.sliit.lk>
2023-04-01 18:54:49 +0530

    ITP

--- frontend/src/Components/Admin/AdminSidebar.js
+++ frontend/src/Components/Admin/AdminSidebar.js
@@ -21,8 +21,8 @@ function AdminSideBar(){
                             </a>
                             <div className="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                                 <nav className="sb-sidenav-menu-nested nav">
-                                    <a className="nav-link" href="" >Add Order</a>
-                                    <a className="nav-link" href="">View Order</a>
+                                    <a className="nav-link" href="" >Add Orders</a>
+                                    <a className="nav-link" href="">View Orders</a>
                                 </nav>
                             </div>
 
